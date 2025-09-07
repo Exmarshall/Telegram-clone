83916b9048891f14c1a03f6d71cefd1394db2030
Exmarshall <umarhayatudeen88@gmail.com>
2025-09-07 03:16:31 +0100

    channel & messsages

--- app/(home)/(tabs)/index.tsx
+++ app/(home)/(tabs)/index.tsx
@@ -1,5 +1,10 @@
-import { Text } from "react-native";
+import { router } from "expo-router";
+import { useState } from "react";
+import { ChannelList } from "stream-chat-expo";
 
 export default function MainTabScreen() {
-  return <Text>Main Tab</Text>;
+  const [channel, setChannel] = useState()
+
+
+  return <ChannelList onSelect={(channel) => router.push(`/channel/${channel.cid}`)} />;
 }
